095cc7daec43e27d8bee441608d47af0e4e724e0
PisethChuon <122347263+PisethChuon@users.noreply.github.com>
2024-07-24 22:28:57 +0700

    page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import PatientForm from "@/components/ui/forms/PatientForm";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -8,12 +10,28 @@ export default function Home() {
           <Image
             src="assets/icons/logo-full.svg"
             height={1000}
-            width={1000}
+            width={3000}
             alt="patient"
-            className="mg-12 h-10 w-fit"
+            className="mg-12 h-10 w-fit"   
           />
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+            © 2024 CarePulse
+            </p>
+            <Link href="/?admin-true" className="text-green-500">
+            Admin
+            </Link>
+          </div>
         </div>
       </section>
+      <Image 
+      src="/assets/images/onboarding-img.png"
+      height={1000}
+      width={1000}
+      alt="patient"
+      className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
