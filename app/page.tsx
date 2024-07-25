05976edc7eb7f6983267d3db79f54d4029e31939
PisethChuon <122347263+PisethChuon@users.noreply.github.com>
2024-07-25 21:34:15 +0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import PatientForm from "@/components/ui/forms/PatientForm";
+import PatientForm from "@/components/forms/PatientForm";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -15,7 +15,6 @@ export default function Home() {
             alt="patient"
             className="mb-12 h-10 w-fit"
           />
-          {/* Patient Form */}
           <PatientForm />
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
