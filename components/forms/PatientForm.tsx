c1b844020f07fed3272a8f87076666ddd86fd080
PisethChuon <122347263+PisethChuon@users.noreply.github.com>
2024-07-31 21:51:34 +0700

    first commit -m

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -3,13 +3,13 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
 import CustomFormField from "../CustomFormField";
 import SubmitButton from "../SubmitButton";
 import { useState } from "react";
 import { UserFormValidation } from "@/lib/validation";
-import { error } from "console";
+// import { error } from "console";
 import { useRouter } from "next/navigation";
 import { createUser } from "@/lib/actions/patient.actions";
 
