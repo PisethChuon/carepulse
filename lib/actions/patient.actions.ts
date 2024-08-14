ccd15fea4bee7b8e8729df5f8d5143e4af8aad19
PisethChuon <122347263+PisethChuon@users.noreply.github.com>
2024-08-14 23:11:20 +0700

    patient action

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -2,6 +2,7 @@
 
 import { ID, Query } from "node-appwrite";
 import { users } from "../appwrite.config";
+import { parseStringify } from "../utils";
 
 export const createUser = async (user: CreateUserParams) => {
   try {
@@ -20,3 +21,13 @@ export const createUser = async (user: CreateUserParams) => {
     }
   }
 };
+
+export const getUser = async (userId: string) => {
+  try {
+    const user = await users.get(userId);
+
+    return parseStringify(user);
+  } catch (error) {
+    console.log(error)
+  }
+}
